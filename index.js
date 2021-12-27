563c13b41bf79f13f24d340b7d87229f417cca44
Zach Levy <zacharyalevy@gmail.com>
2021-12-27 15:06:29 -0500

    respond with any status code in json

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const fs = require('fs');
 const app = express();
 const port = 6000;
 
+function getDebugRequest(req) {
+  const url = new URL(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  return {
+    headers: req.rawHeaders,
+    ...(req.body && { body: req.rawBody.toString('utf8') }),
+    url
+  };
+}
+
 app.get('/', (req, res) => {
   const content = fs.readFileSync('./README.md');
   res.status(200).send(content);
 });
 
+app.get('/responses/:statusCode.json', (req, res) => {
+  const responseBody = {
+    request: getDebugRequest(req)
+  };
+  const statusCode = req.params.statusCode;
+  res.status(statusCode).send(responseBody);
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
